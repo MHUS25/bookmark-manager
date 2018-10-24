0d0782abc18cea79751a4032d54983beda90e283
Muna <mmhussein125@gmail.com>
2018-10-24 18:51:13 +0100

    Updating bookmarks, & now we have a working CRUD app

--- app.rb
+++ app.rb
@@ -21,9 +21,19 @@ class BookmarkManager < Sinatra::Base
     Bookmark.create(url: params[:url], title: params[:title])
     redirect '/bookmarks'
   end
-  
+
   delete '/bookmarks/:id' do
     Bookmark.delete(id: params[:id])
+    redirect '/bookmarks'
+  end
+
+  get '/bookmarks/:id/edit' do
+    @bookmark = Bookmark.find(id: params[:id])
+    erb :'bookmarks/edit'
+  end
+
+  patch '/bookmarks/:id' do
+    Bookmark.update(id: params[:id], title: params[:title], url: params[:url])
     redirect '/bookmarks'
   end
 
