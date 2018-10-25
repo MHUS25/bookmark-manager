3c9b9fcd78cc5d855c5217ffca1ce3d44b21df60
Muna <mmhussein125@gmail.com>
2018-10-25 19:58:55 +0100

    Authentication: Unhappy Path - Wrong password

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -48,4 +48,10 @@ require 'database_helpers'
 
       expect(User.authenticate(email: 'nottherightemail@example.com', password: 'password123')).to be_nil
     end
+
+    it 'returns nil given an incorrect password' do
+      user = User.create(email: 'test@example.com', password: 'password123')
+
+      expect(User.authenticate(email: 'test@example.com', password: 'wrongpassword')).to be_nil
+    end
 end
